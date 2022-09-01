98e4f11a6032440852bd6656e1516c26fe039315
praktykant <praktykant@259AI>
2022-09-01 12:52:59 +0200

    created form employee to add new employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 import { EmployeeAddComponent } from './employees/employee-add/employee-add.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeesComponent,
     EmployeesListComponent,
-    EmployeeAddComponent
+    EmployeeAddComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,15 @@
+export class Employee {
+  name: string;
+  surname: string;
+  dateOfBirth: Date;
+  dataOfEmployment: Date;
+  contract: string;
+
+  constructor(
+    name: string,
+    surname: string,
+    dateOfBirth: Date,
+    dateOfEmployment: Date,
+    contract: string
+  ) {}
+}
